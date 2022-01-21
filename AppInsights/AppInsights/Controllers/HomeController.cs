9caa8276681bf609e0035c77a6a188fd492a053a
Xuhong Liu <xuhliu@microsoft.com>
2022-01-20 20:22:31 -0500

    role

--- AppInsights/AppInsights/Controllers/HomeController.cs
+++ AppInsights/AppInsights/Controllers/HomeController.cs
@@ -14,6 +14,7 @@ namespace AppInsights.Controllers
             var telemetryClient = new TelemetryClient();
             telemetryClient.InstrumentationKey = "489af2eb-1136-4abb-a779-9a56be206db7";
 
+            telemetryClient.Context.Cloud.RoleInstance = "Lucy";
             telemetryClient.TrackEvent("This is a test event");
             telemetryClient.TrackMetric("Test Metric", 777);
             telemetryClient.TrackTrace("Test Trace Message", Microsoft.ApplicationInsights.DataContracts.SeverityLevel.Information);
